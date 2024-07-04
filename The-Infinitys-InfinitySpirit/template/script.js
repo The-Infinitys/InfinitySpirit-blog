8b16df57906685e5c662906e6cfe4ffe7eba2d0b
The Infinity's <137460810+The-Infinitys@users.noreply.github.com>
2024-07-05 08:42:52 +0900

    途中

---
+++ The-Infinitys-InfinitySpirit/template/script.js
@@ -0,0 +1,39 @@
+const testmode = true;
+if (testmode) {
+  const content = document.querySelector("InfinitySpiritContent");
+  content.outerHTML = `
+  <h1>test mode</h1>
+  <p>content</p>
+  <p>content</p>
+  <h2>test mode</h2>
+  <p>content</p>
+  <p>content</p>
+  <p>content</p>
+  <h3>test mode</h3>
+  <p>content</p>
+  <p>content</p>
+  <p>content</p>
+  <p>content</p>
+  <h4>test mode</h4>
+  <p>content</p>
+  <p>content</p>
+  <p>content</p>
+  <p>content</p>
+  <p>content</p>
+  <h5>test mode</h5>
+  <p>content</p>
+  <p>content</p>
+  <p>content</p>
+  <p>content</p>
+  <p>content</p>
+  <p>content</p>
+  <h6>test mode</h6>
+  <p>content</p>
+  <p>content</p>
+  <p>content</p>
+  <p>content</p>
+  <p>content</p>
+  <p>content</p>
+  <p>content</p>
+  `;
+}
