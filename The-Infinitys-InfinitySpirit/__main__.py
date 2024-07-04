47a8a1849be80358be5d561e03526f5bfe9143e7
The-Infinitys <theinfinitysinfinity@gmail.com>
2024-07-04 21:24:54 +0900

    ディレクトリ設定と、設定データの読み込みを適用

--- The-Infinitys-InfinitySpirit/__main__.py
+++ The-Infinitys-InfinitySpirit/__main__.py
@@ -1,14 +1,27 @@
 import os
 import sys
+import json
 
 def main() -> int:
-  def copy_right() -> None:
-    print("∞"*37)
-    print("Infinity Spirit Static Site Generator")
-    print("∞"*8 + "made by The Infinity's" + "∞"*8)
-    print("∞"*37)
-  copy_right()
-  return 0
+    def copy_right() -> None:
+        print("∞" * 41)
+        print("∞Infinity Spirit - Static Site Generator∞")
+        print("∞" * 9 + " made by The Infinity's" + "∞" * 9)
+        print("∞" * 41)
+    if not os.path.isfile("InfinitySpirit.infinity"):
+        print("InfinitySpirit Error: Invalid Current Directory")
+        return 1
+    
+    copy_right()
+    setting=None
+    with open("./The-Infinitys-InfinitySpirit/setting/setting.json") as f:
+        setting=json.loads(f.read())
+    print("Loaded settings")
+    print("-"*40)
+    print("repository name:", setting["git-repository"]["name"])
+    print("repository year:", setting["git-repository"]["year"])
+    return 0
+
 
 if __name__ == "__main__":
     sys.exit(main())
