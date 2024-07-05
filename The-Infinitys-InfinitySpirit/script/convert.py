1b5f26c0181985bd95cf22845a23e310dc8e2968
The Infinity's <137460810+The-Infinitys@users.noreply.github.com>
2024-07-05 02:10:25 +0000

    asdf

--- The-Infinitys-InfinitySpirit/script/convert.py
+++ The-Infinitys-InfinitySpirit/script/convert.py
@@ -1,5 +1,10 @@
 import markdown
 
 template_html = ""
-with open("./The-Infinitys-InfinitySpirit") as f:
-  template_html = f.read()
+with open("./The-Infinitys-InfinitySpirit/template/index.html") as f:
+    template_html = f.read()
+    if (
+        "<InfinitySpiritMetaTitle>" in template_html
+        and "</InfinitySpiritMetaTitle>" in template_html
+    ):
+        print("InfinitySpirit.markdownconverter log: succeeded to load location of meta title")
