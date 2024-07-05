76124db3a600885e0cb4ed8a25a3e2f758efc4a1
The Infinity's <137460810+The-Infinitys@users.noreply.github.com>
2024-07-05 10:57:31 +0900

    complete

--- The-Infinitys-InfinitySpirit/template/script.js
+++ The-Infinitys-InfinitySpirit/template/script.js
@@ -1,10 +1,41 @@
-const generateTitle = () => {
-  const InfinitySpiritTitle = document.querySelector(
-    "InfinitySpiritArticleTitle"
-  );
+const makeIndex = () => {
+  const article_index = document.createElement("div");
+  article_index.className = "article-index";
   const article_body = document.querySelector(".article-body");
-  const titleBox = document.createElement("div");
-  titleBox.innerHTML = InfinitySpiritArticleTitle.innerHTML;
-  titleBox.className = "article-title";
+  const article_content = document.querySelector("InfinitySpiritContent");
+  const addIndex = (element) => {
+    const tag = document.createElement(element.tagName);
+    tag.innerHTML = element.innerHTML;
+    tag.onclick = () => {
+      element.scrollIntoView({
+        behavior: "smooth",
+        block: "end",
+        inline: "center",
+      });
+    };
+    article_index.append(tag);
+  };
+  for (let index = 0; index < article_content.children.length; index++) {
+    const element = article_content.children[index];
+    const tagname = element.tagName.toLowerCase();
+    if (
+      tagname in
+      {
+        h1: "",
+        h2: "",
+        h3: "",
+        h4: "",
+      }
+    ) {
+      addIndex(element);
+    }
+  }
+  article_body.append(article_index);
 };
-generateTitle();
+makeIndex();
+const recommendArticles = () => {
+  const article_list = document.createElement("div");
+  article_list.className = "articles-recommended";
+  document.querySelector(".article-body").append(article_list);
+};
+recommendArticles();
